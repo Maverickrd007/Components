65e4c433ef16e98243574360b0b7864753f8e57c
Maverickrd007 <raghav.duggal.s69@kalvium.community>
2025-02-20 12:16:57 +0530

    done

--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 const Usercard = () => {
-
-  const profilePhoto = "https://via.placeholder.com/100";
-  const name = "John Doe";
-  const email = "johndoe@example.com";
-  const phone = "+1 234 567 890";
-  const address = "123 Main Street, Springfield, USA";
+  
+  const profilePhoto = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bbc.com%2Fnews%2Farticles%2Fcrmnrly0zjeo&psig=AOvVaw2s7ImGxv_X_YIr403XKHqw&ust=1740120283946000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCICNjK7T0YsDFQAAAAAdAAAAABAE";
+  const name = "Ummu woe woe";
+  const email = "wowo@example.com";
+  const phone = "+1 34635635";
+  const address = "1that home 2345";
 
   return (
     <div className="bg-white shadow-lg rounded-2xl p-4 flex flex-col items-center w-80 border border-gray-200">
